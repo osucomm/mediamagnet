e1ad05d4a93d29502a47241fa52e4a0f8de6f561
Mike Butsko <butsko.7@osu.edu>
2014-10-27 15:55:21 -0400

    Routes for channel types

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resources :keywords, only: [:index, :show]
   resources :entities
   resources :channels
+  resources :twitter_channels, controller: 'channels', type: 'Twitter'
+  resources :instagram_channels, controller: 'channels', type: 'Instagram'
+  resources :web_channels, controller: 'channels', type: 'Web'
+  resources :event_channels, controller: 'channels', type: 'Event'
 
   root 'welcome#show'
 end
