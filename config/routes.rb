3a25b8d3c936e14e837c02f4c4b35f12c3660429
Mike Butsko <butsko.7@osu.edu>
2014-10-27 10:22:02 -0400

    Install pundit gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'entities/index'
+
+  get 'entities/show'
+
+  get 'entities/new'
+
+  get 'entities/create'
+
+  get 'entities/edit'
+
+  get 'entities/update'
+
+  get 'entities/destroy'
+
   devise_for :users
   get 'dashboard' => 'dashboard#show'
 
