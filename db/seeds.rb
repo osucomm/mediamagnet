36e5d4a321c80a3f3b7686310b4e21f50bd25095
Mike Butsko <butsko.7@osu.edu>
2014-10-27 14:11:34 -0400

    Seed Jim, Corey and Mike if they don't already exist

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+puts 'Creating admins, needed for entites'
+
+%w(butsko.7 hinshaw.25 burgoon.5).each do |name_n|
+
+  u = User.where(
+    email: "#{name_n}@osu.edu"
+  ).first_or_initialize
+
+  if u.new_record?
+    tmp_pass = (0...12).map { (65 + rand(26)).chr }.join
+    u.password = tmp_pass
+    u.encrypted_password = tmp_pass
+    u.save!
+    puts "Created user #{u.email}"
+  end
+end
 
 puts 'Creating entites...'
 
-Entity.where(
+e = Entity.where(
     name: 'University Communications',
     description: 'Central communications office',
     link: 'http://ucom.osu.edu',
 ).first_or_create
+
 Entity.where(
     name: 'Arts and Sciences Communications',
     description: 'College comm office',
