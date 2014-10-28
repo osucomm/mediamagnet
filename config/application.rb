51d0669aac5b7023675c8760ae3a0a306a9664e4
Mike Butsko <butsko.7@osu.edu>
2014-10-28 15:33:23 -0400

    Vendor assets, not app asets

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Mediamagnet
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.paths << Rails.root.join('app', 'assets', 'components')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
 
     config.autoload_paths += Dir[
       "#{config.root}/app/models/**/",
