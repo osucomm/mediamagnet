b14f01f3fcef987081a47c5d88d54a020647a29e
Mike Butsko <butsko.7@osu.edu>
2014-10-29 14:15:41 -0400

    Basic working method to create items from tweets

--- app/models/channels/twitter_channel.rb
+++ app/models/channels/twitter_channel.rb
@@ -2,4 +2,33 @@ class TwitterChannel < Channel
   def service_id_name
     'Handle'
   end
+
+  def client
+    @client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+    end
+  end
+
+  def run
+    new_count = 0
+
+    # Get tweets from our user.
+    tweets = client.user_timeline(name)
+
+    #Check tweet identifiers against 
+    tweets.each do |tweet|
+      unless items.where(guid: tweet.id).exists?
+        items.create(
+          guid: tweet.id,
+          description: tweet.text,
+          published_at: created_at
+        )
+      end
+    end
+    logger.info "Channel #{name} updated with #{new_count} new items."
+  end
+
 end
