be80cf72851ea01a3b96092e6036b8d1f27493d2
Corey Hinshaw <hinshaw.25@osu.edu>
2014-10-29 01:12:19 -0400

    Fixed bootstrap flash alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def flash_class level
+    case level
+      when 'notice' then 'alert-info'
+      when 'succes' then 'alert-success'
+      when 'error' then 'alert-danger'
+      when 'alert' then 'alert-warning'
+    end
+  end
+
 end
