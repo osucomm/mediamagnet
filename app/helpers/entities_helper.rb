a22559d82c1ae5ab6a1642f41119ec6321cdcbbd
Mike Butsko <butsko.7@osu.edu>
2014-10-30 13:32:47 -0400

    Ask for confirmation before deleting entity

--- app/helpers/entities_helper.rb
+++ app/helpers/entities_helper.rb
@@ -3,13 +3,14 @@ module EntitiesHelper
   def entity_action_links(entity)
     links = []
     if current_user.entities.exclude?(entity)
-      links << (link_to 'Join', entity_path(entity), method: :delete)
+      links << (link_to 'Join', entity_path(entity))
     end
     if entity.has_user? current_user
       links << (link_to 'Edit', edit_entity_path(entity))
     end
     if current_user.admin?
-      links << (link_to 'Destroy', entity_path(entity), method: :delete)
+      links << (link_to 'Destroy', entity_path(entity), method: :delete, 
+                data: { confirm: 'Are you sure?' })
     end
     links.join(' | ').html_safe
   end
