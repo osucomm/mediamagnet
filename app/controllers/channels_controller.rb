f0d71018e0ece72ae37e0f7404b443b420966451
Corey Hinshaw <hinshaw.25@osu.edu>
2014-10-29 20:46:55 -0400

    Only authenticate users when editing a channel

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,6 +1,6 @@
 class ChannelsController < ApplicationController
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :find_channel, only: [:show, :edit, :update, :destroy]
 
   respond_to :html, :xml, :json, :js
