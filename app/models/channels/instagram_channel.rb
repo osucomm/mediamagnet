baca2410b22ce3084618b6c4d8a63cbaeebf31bf
Mike Butsko <butsko.7@osu.edu>
2014-11-05 14:31:20 -0500

    Instagram creates items now

--- app/models/channels/instagram_channel.rb
+++ app/models/channels/instagram_channel.rb
@@ -1,7 +1,10 @@
 class InstagramChannel < Channel
 
+  # Callbacks
+  # Instagram endpoint queries are based on numeric user id, so lets set that.
   before_save :set_service_identifier_id
 
+  # Validations
   validate :is_instagram_user
 
   def service_id_name
@@ -16,51 +19,54 @@ class InstagramChannel < Channel
           config.client_secret   = ENV['INSTAGRAM_CLIENT_SECRET']
         end
         Instagram.client
-     end
+      end
+  end
+
+  def new_media
+    client.user_recent_media(service_identifier_id,
+                             min_id: (most_recent_item_id.to_i + 1))
   end
 
 
   def refresh_items
-    @new_count = 0
-
-    # Get tweets from our user, starting with the one after the last one we have.
-    # media = client.user_timeline(name, since_id: items.most_recent.first.guid)
-
-    #Check tweet identifiers against 
-    #tweets.each do |tweet|
-    #  unless items.where(guid: tweet.id.to_s).exists?
-    #    items.create(
-    #      guid: tweet.id,
-    #      description: tweet.text,
-    #      published_at: tweet.created_at
-    #    )
-    #  end
-    #end
-    #true
-    #super
-  end
-
-  def link_for(item)
-    "https://twitter.com/#{service_identifier}/status/#{item.guid}"
+    new_media.each do |media|
+      unless items.where(guid: media.id.to_s).exists?
+        items.create(
+          guid: media.id,
+          link: media.link,
+          description: (media.caption? ? media.caption.text : ''),
+          published_at: Date.strptime(media.created_time, '%s')
+        )
+      end
+    end
+    super
   end
 
   private
 
   def set_service_identifier_id
-    service_identifer_id = matching_users.first.id
+    self.service_identifier_id = instagram_user.id
   end
 
   def is_instagram_user
-    if matching_users.count == 1
-      matching_users.first.id
-    else
+    unless instagram_user
       errors.add :service_identifier, ' must match current instagram account'
     end
   end
 
-  def matching_users
-    client.user_search(service_identifier).select do |instagram_user|
+  def instagram_user
+    users = client.user_search(service_identifier).select do |instagram_user|
       instagram_user.username == service_identifier
+    end.first
+  end
+
+  def most_recent_item_id
+    # Although api service [media_id]_[user_id] format, it expects queries
+    # based off of just [media_id].
+    if items.most_recent.any? 
+      items.most_recent.first.guid.split('_').first
+    else
+      0
     end
   end
 
