58c23a86763113715a9ab87d2685f70c5bc2adfb
Mike Butsko <butsko.7@osu.edu>
2014-11-06 10:06:04 -0500

    Initialize youtubeit client

--- app/models/channels/youtube_channel.rb
+++ app/models/channels/youtube_channel.rb
@@ -2,4 +2,9 @@ class YoutubeChannel < Channel
   def service_id_name
     'Channel name'
   end
+
+  def client
+    @client ||= YouTubeIt::Client.new(:dev_key => ENV['YOUTUBE_API_KEY'])
+  end
+
 end
