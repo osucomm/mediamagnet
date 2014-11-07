197fabe4ce7a07e4a371a1fe5a3afae2ece922f0
Corey Hinshaw <hinshaw.25@osu.edu>
2014-11-07 14:11:04 -0500

    Added items route to channels

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,14 @@ Rails.application.routes.draw do
   resources :items, only: [:index, :show]
   resources :users, only: [:index, :update]
 
-  resources :channels, only: [:index, :show, :destroy]
+  resources :channels, only: [:index, :show, :destroy] do
+    resources :items, only: [:index, :show]
+  end
+
   Channel::TYPES.each do |type|
-    resources type.model_name.plural, only: :index, controller: 'channels', type: type.to_s
+    resources type.model_name.plural, only: :index, controller: 'channels', type: type.to_s do
+      resources :items, only: [:index, :show]
+    end
   end
 
   resources :entities, shallow: true do
