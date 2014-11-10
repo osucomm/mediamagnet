594f637003317a46816ee76a43a03561ee097857
Mike Butsko <butsko.7@osu.edu>
2014-11-10 16:32:01 -0500

    Add delayed_job_active_record and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141107194108) do
+ActiveRecord::Schema.define(version: 20141110213117) do
 
   create_table "assets", force: true do |t|
     t.integer  "item_id"
@@ -58,6 +58,22 @@ ActiveRecord::Schema.define(version: 20141107194108) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
 
   create_table "entities", force: true do |t|
     t.string   "name"
