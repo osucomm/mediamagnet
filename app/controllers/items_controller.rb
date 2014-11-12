a1bb9a4f9a1e7119bc1ad349efe67f3f584c27d7
Corey Hinshaw <hinshaw.25@osu.edu>
2014-11-12 11:30:51 -0500

    Add items RSS feed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,19 @@
 class ItemsController < ApplicationController
+  respond_to :html, :js, :json
+  respond_to :rss, only: :index
+
   def index
     @items = Item
       .includes(:channel)
       .page(params[:page])
       .per(params[:per])
+
+    @channel = Channel.find(params[:channel_id]) if params[:channel_id]
+    respond_with @items
   end
 
   def show
+    @item = Item.includes(:channel).find(params[:id])
+    respond_with @items
   end
 end
