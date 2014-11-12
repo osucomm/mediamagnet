dbf14e80e9934b80a210ade3c400ccba65f3b8a0
Corey Hinshaw <hinshaw.25@osu.edu>
2014-11-12 12:28:18 -0500

    Fixed password edit form

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
 
   devise_for :users, path: 'user', :skip => [:registrations] 
   as :user do
-      get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-        put 'users' => 'devise/registrations#update', :as => 'user_registration'
+    get 'user/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'user' => 'devise/registrations#update', :as => 'user_registration'
   end
+
   get 'dashboard' => 'dashboard#show', as: :dashboard
 
   resources :keywords
