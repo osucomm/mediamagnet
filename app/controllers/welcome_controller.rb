b3b44d7a107ce5be189acd22615ee154653afdaa
Mike Butsko <butsko.7@osu.edu>
2014-11-23 17:15:31 -0500

    Redirect to entities list if user doesn't have a current_entity

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   skip_before_filter :disallow_blocked, only: :show
 
   def show
-    redirect_to current_user.current_entity if current_user
+    path = current_user.current_entity || entities_path
+    redirect_to path if current_user
   end
 end
