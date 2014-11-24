1df55f425ec2e0ece9895db4b8f287a637cf35f6
Mike Butsko <butsko.7@osu.edu>
2014-11-24 08:45:41 -0500

    Proper redirects whether logged in or logged out, skip dashboards for now

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    path = current_user.current_entity || entities_path
+    redirect_to path
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,6 @@ class WelcomeController < ApplicationController
   skip_before_filter :disallow_blocked, only: :show
 
   def show
-    path = current_user.current_entity || entities_path
-    redirect_to path if current_user
+    redirect_to dashboard_path if user_signed_in?
   end
 end
