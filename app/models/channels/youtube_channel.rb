577c27bc7bdaac14176f20e5f42ba203b0f1b4b2
Mike Butsko <butsko.7@osu.edu>
2014-11-24 14:00:34 -0500

    Missing message partial

--- app/models/channels/youtube_channel.rb
+++ app/models/channels/youtube_channel.rb
@@ -1,3 +1,6 @@
+require 'google/api_client'
+require 'google/api_client/auth/installed_app'
+
 class YoutubeChannel < Channel
   def service_id_name
     'Channel name'
@@ -7,8 +10,44 @@ class YoutubeChannel < Channel
     'youtube-play'
   end
 
-  def client
-    @client ||= YouTubeIt::Client.new(:dev_key => ENV['YOUTUBE_API_KEY'])
+  def videos
+    request = {
+      api_method: youtube.search.list,
+      authorization: nil,
+      :parameters => {part: 'snippet', channelId: service_identifier}
+    }
+
+    youtube_videos = []
+    loop do
+      result = client.execute(request)
+
+      youtube_videos.concat(result.data.items)
+
+      break unless result.next_page_token
+      request = result.next_page
+    end
+    youtube_videos
+  end
+
+  def refresh_items
+    videos.each do |youtube_video|
+      unless items.where(guid: youtube_video.id.videoId).any?
+        i = items.build(
+          guid: youtube_video.id.videoId,
+          title: youtube_video.snippet.title,
+          description: youtube_video.snippet.description,
+          link: ''
+        )
+      i.tags
+      i.assets.build(url: youtube_video.snippet.thumbnails.high.url)
+      end
+    end
+  end
+
+  private
+
+  def youtube
+    @youtube ||= client.discovered_api('youtube', 'v3')
   end
 
 end
