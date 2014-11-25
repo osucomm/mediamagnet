c71e60c9a9241c79bd723892a86a3d39af818d14
Mike Butsko <butsko.7@osu.edu>
2014-11-25 14:21:09 -0500

    Events channel creation

--- app/models/channels/event_channel.rb
+++ app/models/channels/event_channel.rb
@@ -12,4 +12,31 @@ class EventChannel < Channel
   def service_id_name
     'Feed URL'
   end
+
+  def client
+    @client ||= Feedjira::Feed.fetch_and_parse(service_identifier)
+  end
+
+  def refresh_items
+    client.entries.each do |entry|
+      unless items.where(guid: entry.entry_id).exists?
+        item = items.create(
+          guid: entry.entry_id,
+          title: entry.title,
+          content: entry.content,
+          description: entry.summary,
+          link: entry.url,
+          published_at: entry.published
+        )
+        item.tag_names = (entry.categories) if entry.categories
+        e = item.events.build(
+          start_date: entry.start_date,
+          end_date: entry.end_date,
+        )
+        e.location = Location.where(location: entry.location).first_or_create
+        e.save
+      end
+    end
+    super
+  end
 end
