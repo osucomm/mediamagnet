b58c3a8956266047bbeea95f10f48f520e02ebf4
Mike Butsko <butsko.7@osu.edu>
2014-12-09 13:07:47 -0500

    Link resolvers and regex for detecting links in text

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,8 +3,33 @@ class Link < ActiveRecord::Base
   has_many :items, through: :item_links
   belongs_to :item
 
+  PATTERN = /https?:\/\/[\da-z\.-]+\.[a-z\.]{2,6}[\/\w\.-]*\/?/
+
   def to_s
     url
+  end
+
+  class << self
+    def resolve_uri(url)
+      binding.pry
+      uri = URI(url.strip)
+      return_url = ''
+
+      Net::HTTP.start(uri.host, uri.port,
+                        :use_ssl => uri.scheme == 'https') do |http|
+          request = Net::HTTP::Get.new uri
+          response = http.request request # Net::HTTPResponse object
+          if response.code.to_s.match(/^3/)
+            return_url = resolve_uri(response['location'])
+          elsif response.code.to_s.match(/^2/)
+            return_url = response.uri.to_s
+          else
+            return_url = nil
+          end
+
+      end
+    return_url
+    end
   end
 
 end
--- lib/tasks/items.rake
+++ lib/tasks/items.rake
@@ -2,10 +2,21 @@ namespace :items do
   desc 'Create links from link string fields on items'
   task buildlinks: :environment do
 
-    Item.each do |item|
+    Item.all.each do |item|
       item.link = Link.where(url: item.attributes['link']).create if item.link.nil?
       item.save!
     end
 
   end
+
+  desc 'Create links from link string fields on items'
+  task parselinks: :environment do
+
+    Item.all.each do |item|
+      item.send(:links_from_text_fields)
+      item.save!
+    end
+
+  end
+
 end
