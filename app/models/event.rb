8f18162fc2589c1554ebe9ecdf6bf96c45fa67ab
Mike Butsko <butsko.7@osu.edu>
2014-12-11 12:32:29 -0500

    Help text for event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,15 @@
 class Event < ActiveRecord::Base
   belongs_to :location
 
+  class << self
+    def help_text
+      <<-EOT
+        Items may have associated events which have start date/time, end date/time
+        and location.
+      EOT
+    end
+  end
+
   def location_name
     location.location if location
   end
