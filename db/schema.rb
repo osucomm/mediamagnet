c302171f577bc9d449fd2cb96382a18ded5c3cc6
Mike Butsko <butsko.7@osu.edu>
2015-01-09 16:27:38 -0500

    Forgot schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150107155848) do
+ActiveRecord::Schema.define(version: 20150109211419) do
 
-  create_table "assets", force: true do |t|
-    t.integer  "item_id"
+  create_table "assets", force: :cascade do |t|
+    t.integer  "item_id",    null: false
     t.string   "mime"
-    t.string   "url"
+    t.string   "url",        null: false
     t.string   "title"
     t.string   "alt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "channels", force: true do |t|
-    t.string   "type"
-    t.string   "name"
+  add_index "assets", ["item_id"], name: "index_assets_on_item_id"
+
+  create_table "channels", force: :cascade do |t|
+    t.string   "type",                  null: false
+    t.string   "name",                  null: false
     t.text     "description"
-    t.integer  "entity_id"
+    t.integer  "entity_id",             null: false
     t.boolean  "primary"
     t.string   "service_identifier"
     t.string   "url"
@@ -37,29 +39,24 @@ ActiveRecord::Schema.define(version: 20150107155848) do
     t.integer  "service_identifier_id"
   end
 
-  create_table "channels_keywords", force: true do |t|
-    t.integer "channel_id"
-    t.integer "keyword_id"
-  end
+  add_index "channels", ["entity_id"], name: "index_channels_on_entity_id"
+  add_index "channels", ["type"], name: "index_channels_on_type"
 
-  create_table "channels_tags", force: true do |t|
-    t.integer "channel_id"
-    t.integer "tag_id"
-  end
-
-  create_table "contacts", force: true do |t|
+  create_table "contacts", force: :cascade do |t|
     t.string   "name"
     t.string   "organization"
     t.string   "url"
     t.string   "phone"
     t.string   "email"
-    t.integer  "contactable_id"
-    t.string   "contactable_type"
+    t.integer  "contactable_id",   null: false
+    t.string   "contactable_type", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "delayed_jobs", force: true do |t|
+  add_index "contacts", ["contactable_id", "contactable_type"], name: "index_contacts_on_contactable_id_and_contactable_type"
+
+  create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
     t.integer  "attempts",   default: 0, null: false
     t.text     "handler",                null: false
@@ -75,8 +72,8 @@ ActiveRecord::Schema.define(version: 20150107155848) do
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
 
-  create_table "entities", force: true do |t|
-    t.string   "name"
+  create_table "entities", force: :cascade do |t|
+    t.string   "name",        null: false
     t.text     "description"
     t.string   "link"
     t.datetime "created_at"
@@ -86,20 +83,18 @@ ActiveRecord::Schema.define(version: 20150107155848) do
 
   add_index "entities", ["parent_id"], name: "index_entities_on_parent_id"
 
-  create_table "entities_keywords", force: true do |t|
-    t.integer "entity_id"
-    t.integer "keyword_id"
-  end
-
-  create_table "entities_users", force: true do |t|
-    t.integer  "entity_id"
-    t.integer  "user_id"
+  create_table "entities_users", force: :cascade do |t|
+    t.integer  "entity_id",  null: false
+    t.integer  "user_id",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "events", force: true do |t|
-    t.integer  "item_id"
+  add_index "entities_users", ["entity_id"], name: "index_entities_users_on_entity_id"
+  add_index "entities_users", ["user_id"], name: "index_entities_users_on_user_id"
+
+  create_table "events", force: :cascade do |t|
+    t.integer  "item_id",     null: false
     t.integer  "location_id"
     t.datetime "start_date"
     t.datetime "end_date"
@@ -107,9 +102,14 @@ ActiveRecord::Schema.define(version: 20150107155848) do
     t.datetime "updated_at"
   end
 
-  create_table "items", force: true do |t|
+  add_index "events", ["end_date"], name: "index_events_on_end_date"
+  add_index "events", ["item_id"], name: "index_events_on_item_id"
+  add_index "events", ["location_id"], name: "index_events_on_location_id"
+  add_index "events", ["start_date"], name: "index_events_on_start_date"
+
+  create_table "items", force: :cascade do |t|
     t.string   "title"
-    t.integer  "channel_id"
+    t.integer  "channel_id",   null: false
     t.text     "description"
     t.string   "guid"
     t.datetime "published_at"
@@ -119,9 +119,12 @@ ActiveRecord::Schema.define(version: 20150107155848) do
     t.text     "content"
   end
 
-  create_table "items_links", force: true do |t|
-    t.integer  "item_id"
-    t.integer  "link_id"
+  add_index "items", ["channel_id"], name: "index_items_on_channel_id"
+  add_index "items", ["published_at"], name: "index_items_on_published_at"
+
+  create_table "items_links", force: :cascade do |t|
+    t.integer  "item_id",    null: false
+    t.integer  "link_id",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -129,10 +132,10 @@ ActiveRecord::Schema.define(version: 20150107155848) do
   add_index "items_links", ["item_id"], name: "index_items_links_on_item_id"
   add_index "items_links", ["link_id"], name: "index_items_links_on_link_id"
 
-  create_table "keyword_usages", force: true do |t|
-    t.integer  "keyword_id"
-    t.integer  "channel_id"
-    t.integer  "count"
+  create_table "keyword_usages", force: :cascade do |t|
+    t.integer  "keyword_id",              null: false
+    t.integer  "channel_id",              null: false
+    t.integer  "count",       default: 0
     t.datetime "starts_at"
     t.datetime "ends_at"
     t.integer  "week_number"
@@ -140,34 +143,41 @@ ActiveRecord::Schema.define(version: 20150107155848) do
   end
 
   add_index "keyword_usages", ["channel_id"], name: "index_keyword_usages_on_channel_id"
+  add_index "keyword_usages", ["count"], name: "index_keyword_usages_on_count"
   add_index "keyword_usages", ["keyword_id"], name: "index_keyword_usages_on_keyword_id"
 
-  create_table "keywordings", force: true do |t|
-    t.string   "keywordable_type"
-    t.integer  "keywordable_id"
-    t.integer  "keyword_id"
+  create_table "keywordings", force: :cascade do |t|
+    t.string   "keywordable_type", null: false
+    t.integer  "keywordable_id",   null: false
+    t.integer  "keyword_id",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "keywords", force: true do |t|
-    t.string   "name"
+  add_index "keywordings", ["keyword_id"], name: "index_keywordings_on_keyword_id"
+  add_index "keywordings", ["keywordable_id", "keywordable_type"], name: "index_keywordings_on_keywordable_id_and_keywordable_type"
+
+  create_table "keywords", force: :cascade do |t|
+    t.string   "name",         null: false
     t.string   "display_name"
     t.text     "description"
     t.integer  "category"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "item_counter"
   end
 
-  create_table "links", force: true do |t|
-    t.string   "url"
-    t.integer  "item_id"
+  add_index "keywords", ["name"], name: "index_keywords_on_name"
+
+  create_table "links", force: :cascade do |t|
+    t.string   "url",        null: false
+    t.integer  "item_id",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "locations", force: true do |t|
+  add_index "links", ["item_id"], name: "index_links_on_item_id"
+
+  create_table "locations", force: :cascade do |t|
     t.string   "location"
     t.float    "latitude"
     t.float    "longitude"
@@ -175,9 +185,9 @@ ActiveRecord::Schema.define(version: 20150107155848) do
     t.datetime "updated_at"
   end
 
-  create_table "manifests", force: true do |t|
-    t.integer  "entity_id"
-    t.string   "url"
+  create_table "manifests", force: :cascade do |t|
+    t.integer  "entity_id",     null: false
+    t.string   "url",           null: false
     t.text     "last_response"
     t.datetime "succeded_at"
     t.datetime "fetched_at"
@@ -185,40 +195,52 @@ ActiveRecord::Schema.define(version: 20150107155848) do
     t.datetime "updated_at"
   end
 
-  create_table "mappings", force: true do |t|
-    t.integer  "mappable_id"
-    t.string   "mappable_type"
-    t.integer  "tag_id"
-    t.integer  "keyword_id"
+  add_index "manifests", ["entity_id"], name: "index_manifests_on_entity_id"
+
+  create_table "mappings", force: :cascade do |t|
+    t.integer  "mappable_id",   null: false
+    t.string   "mappable_type", null: false
+    t.integer  "tag_id",        null: false
+    t.integer  "keyword_id",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "type"
   end
 
-  create_table "taggings", force: true do |t|
-    t.integer "tag_id"
-    t.integer "item_id"
+  add_index "mappings", ["keyword_id"], name: "index_mappings_on_keyword_id"
+  add_index "mappings", ["mappable_id", "mappable_type"], name: "index_mappings_on_mappable_id_and_mappable_type"
+  add_index "mappings", ["tag_id"], name: "index_mappings_on_tag_id"
+  add_index "mappings", ["type"], name: "index_mappings_on_type"
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_id",  null: false
+    t.integer "item_id", null: false
   end
 
-  create_table "tags", force: true do |t|
-    t.string   "name"
+  add_index "taggings", ["item_id"], name: "index_taggings_on_item_id"
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "tokens", force: true do |t|
+  create_table "tokens", force: :cascade do |t|
     t.string   "access_token"
     t.string   "refresh_token"
     t.string   "expires_at"
-    t.integer  "channel_id"
+    t.integer  "channel_id",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "provider"
   end
 
-  create_table "users", force: true do |t|
+  add_index "tokens", ["channel_id"], name: "index_tokens_on_channel_id"
+
+  create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "email"
+    t.string   "email",                                  null: false
     t.string   "fullname"
     t.datetime "last_sign_in_at"
     t.datetime "created_at"
@@ -239,7 +261,9 @@ ActiveRecord::Schema.define(version: 20150107155848) do
     t.integer  "current_entity_id"
   end
 
+  add_index "users", ["admin"], name: "index_users_on_admin"
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username"
 
 end
