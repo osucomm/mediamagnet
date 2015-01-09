63d8a99f7aeee96ffa35edee6096b8aa62968fae
Corey Hinshaw <hinshaw.25@osu.edu>
2015-01-09 14:45:06 -0500

    Added user tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,18 @@ FactoryGirl.define do
     fullname      { Faker::Name.name }
     password      { '123abc123' }
     password_confirmation      { '123abc123' }
+
+    trait :is_admin do
+      admin { true }
+    end
+
+    trait :is_blocked do
+      blocked { true }
+    end
+
+    trait :without_name do
+      fullname { nil }
+    end
+
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,31 @@
 require 'spec_helper'
 
 describe User do
+
+  context '#is_admin?' do
+    let(:user) { create(:user) }
+    let(:admin_user) { create(:user, :is_admin) }
+
+    it 'returns true when user is an admin' do
+      expect(admin_user.is_admin?).to eql(true)
+    end
+
+    it 'returns false when user is not an admin' do
+      expect(user.is_admin?).to eql(false)
+    end
+  end
+
+  describe 'display name' do
+    let(:user) { create(:user) }
+    let(:user_without_name) { create(:user, :without_name) }
+
+    it 'is the full name if present' do
+      expect(user.display_name).to eql(user.fullname)
+    end
+
+    it 'is the username when there is no full name' do
+      expect(user_without_name.display_name).to eql(user_without_name.username)
+    end
+  end
+
 end
