9703ba436634c696e0087d3bc40924802ae7aee1
Corey Hinshaw <hinshaw.25@osu.edu>
2015-01-09 13:26:31 -0500

    Replace fixture tests with factories

--- spec/models/entity_spec.rb
+++ spec/models/entity_spec.rb
@@ -1,24 +1,25 @@
 require 'spec_helper'
 
 describe Entity do
+  let(:entity) { FactoryGirl.create(:entity) }
+
   describe 'associations' do
-    it { should have_many :entity_mappings }
+    it { should have_many :mappings }
     it { should respond_to :channels_mappings }
   end
 
-  it 'should return false when the user is a member of the entity' do
-    expect(entities(:ucomm).name).to eql(false)
-  end
 
-  #
-  describe '#has_user?' do
+  context '#has_user?' do
+    let(:user) { FactoryGirl.create(:user) }
 
     it 'should return true when the user is a member of the entity' do
       entity.users << user
       expect(entity.has_user?(user)).to eql(true)
     end
 
-
+    it 'should return false when the user is not a member of the entity' do
+      expect(entity.has_user?(user)).to eql(false)
+    end
   end
 
 end
