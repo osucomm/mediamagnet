4d7f2e3d9461072609533128a34eeb34cd34ba28
Mike Butsko <butsko.7@osu.edu>
2015-01-12 16:37:49 -0500

    Use published_at instead of created_at

--- app/models/item.rb
+++ app/models/item.rb
@@ -34,8 +34,8 @@ class Item < ActiveRecord::Base
   scope :by_channels, -> channel_ids { where(channel_id: channel_ids) }
   scope :by_keywords, -> keyword_ids { joins(:keywordings).where('keywordings.keyword_id' => keyword_ids) }
   scope :between, -> (starts_at, ends_at) { after(starts_at).before(ends_at) }
-  scope :before, -> (datetime) { where('created_at < ?', datetime) }
-  scope :after, -> (datetime) { where('created_at > ?', datetime) }
+  scope :before, -> (datetime) { where('published_at < ?', datetime) }
+  scope :after, -> (datetime) { where('published_at > ?', datetime) }
 
   class << self 
     def keywords
