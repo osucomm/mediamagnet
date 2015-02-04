8dbef89aa1d46083cf994d559939c5eb5bd32d62
Mike Butsko <butsko.7@osu.edu>
2015-02-04 10:28:43 -0500

    Remove old code

--- app/models/mappings/channel_mapping.rb
+++ app/models/mappings/channel_mapping.rb
@@ -1,15 +1,3 @@
 class ChannelMapping < Mapping
 
-  before_destroy :remove_keyword_taggings
-
-  private
-
-  def remove_keyword_taggings
-    if new_keyword_id = mappable.entity_mappings.where(tag_id: tag.id).first.try(:keyword_id)
-      Tagging.by_mappable(mappable).by_tag(tag).
-        update_all(keyword_id: new_keyword_id)
-    end
-  end
-
-
 end
