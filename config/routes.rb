09a0397bb6e74905d08eb3fbdae3a52938172930
Mike Butsko <butsko.7@osu.edu>
2015-02-10 15:03:23 -0500

    Add routes for keywordings

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :keywords
   resources :links
   resources :categories, only: [:index]
+  resources :keywordings, only: [:new, :create, :destroy]
 
   #reports
   get 'reports/keyword_usage'
