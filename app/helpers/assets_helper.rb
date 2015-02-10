814bba6567d29e718c141b00e4990fab6d5efae5
Mike Butsko <butsko.7@osu.edu>
2015-02-10 14:58:18 -0500

    Updated to item rendering

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -3,7 +3,7 @@ module AssetsHelper
   def render_asset(asset)
     case(asset.mime.split('/').first)
     when 'image'
-      content_tag(:img, nil, src: asset.url, alt: asset.alt, title: asset.title)
+      content_tag(:img, nil, src: asset.url, alt: asset.alt, title: asset.title, class: 'media-object')
     else
       link_to asset.url, asset.url
     end
