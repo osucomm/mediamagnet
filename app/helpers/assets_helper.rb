354512b5f7f019f2c17c4590f48ff135895d4d33
Corey Hinshaw <hinshaw.25@osu.edu>
2015-02-12 20:01:46 -0500

    Make custom_class optional in render_asset helper

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -1,9 +1,9 @@
 module AssetsHelper
 
-  def render_asset(asset,custom_class)
+  def render_asset(asset, custom_class=nil)
     case(asset.mime.split('/').first)
     when 'image'
-      content_tag(:img, nil, src: asset.url, alt: asset.alt, title: asset.title, class: 'media-object '+custom_class)
+      content_tag(:img, nil, src: asset.url, alt: asset.alt, title: asset.title, class: ['media-object', custom_class].join(' '))
     else
       link_to asset.url, asset.url
     end
