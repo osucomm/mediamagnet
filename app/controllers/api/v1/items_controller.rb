27acb48b5c8bf9f8ca3cc50029f965b0b705d00a
Corey Hinshaw <hinshaw.25@osu.edu>
2015-02-17 15:53:01 -0500

    Start API params

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::ItemsController < ApplicationController
   respond_to :json, :xml
 
   def index
-    @items = Item.search( *Item.search_params(params) )
+    q = params[:search]
+    options = {}
+    options['tag_names'] = params[:tags] if params[:tags]
+
+    #@items = Item.search( *Item.search_params(params) )
+    @items = Item.search( q, options )
       .page(params[:page]).per(params[:per_page]).records
         .eager_load(:assets, :link, :channel, :keywords)
         .from_approved
