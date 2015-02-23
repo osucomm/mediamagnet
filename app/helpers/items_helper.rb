d515b94da75b8a09494a32f2a513ccc406084e2e
Mike Butsko <butsko.7@osu.edu>
2015-02-23 13:10:58 -0500

    Definitions for excerpt and content

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -8,7 +8,23 @@ module ItemsHelper
     end
   end
 
-  def short_description(item)
-    truncate(strip_tags(item.description.html_safe).html_safe, length: 150)
+  def excerpt(item, length=140)
+    truncate(strip_tags(item.to_s.html_safe).html_safe, length: length)
+  end
+
+  def content(item)
+    if item.content.nil?
+    # Channel-specific html
+      case item.channel_type
+      when 'youtube'
+        "<iframe src=\"https://www.youtube.com/embed/#{item.guid}\" />".html_safe
+      when 'instagram'
+        "<img src=\"https://www.youtube.com/embed/#{item.assets.first.url}\" />".html_safe
+      else
+        auto_link(item.to_s)
+      end
+    else
+      item.content.html_safe
+    end
   end
 end
