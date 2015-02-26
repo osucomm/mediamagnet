7bb55ef5c1432e9d6c222c2c6e40e7d90515c2ad
Mike Butsko <butsko.7@osu.edu>
2015-02-26 13:41:10 -0500

    Remove cruft

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,13 +11,12 @@ class ItemsController < ApplicationController
   def index
     search = Item.search( *search_params(params) )
     @search_response = search.response
-    @items = search.page(params[:page]).per(params[:per_page]).records
+    @items = search.records
         .eager_load(:assets, :link, :channel, :keywords)
         .from_approved
         .page(params[:page]).per(params[:per_page])
 
     @api_url = api_url
-    @human_desc = human_desc
     authorize @items
 
     respond_with @items
@@ -51,25 +50,9 @@ class ItemsController < ApplicationController
           "#{k}[]=#{tag}"
         end.join('&')
       else
-        "#{k}=#{v.first}"
+        "#{k}=#{v.class == Array ? v.first : v}"
       end
     end.join('&')
   end
-
-  def human_desc
-    p = params.dup
-    api_params = Item.send(:search_facet_fields).concat(['search'])
-    p.keep_if {|k,v| api_params.include?(k) && v.present? }
-    p.map do |k,v|
-      if v.class == Array && v.length > 1
-        v.map do |tag|
-          "#{tag}"
-        end.join(' or ').prepend("#{k} is ")
-      else
-        "#{k} is #{v.first}"
-      end
-    end.join(' and ')
-  end
-
 
 end
