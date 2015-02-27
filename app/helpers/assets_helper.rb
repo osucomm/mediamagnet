770c711f6f686e01f422fbcecb6eda4ba1ac9800
Mike Butsko <butsko.7@osu.edu>
2015-02-27 14:11:57 -0500

    Instagram videos

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -4,6 +4,8 @@ module AssetsHelper
     case(asset.mime.split('/').first)
     when 'image'
       content_tag(:img, nil, src: asset.url, alt: asset.alt, title: asset.title, class: ['media-object', custom_class].join(' '))
+    when 'video'
+      content_tag(:video, nil, src: asset.url, controls: true)
     else
       link_to asset.url, asset.url
     end
