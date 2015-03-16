6dce79fdfdf49246512c146f774fcce4276e70e4
Mike Butsko <butsko.7@osu.edu>
2015-03-16 12:56:49 -0400

    Digest with blank fields

--- app/models/channels/rss_channel.rb
+++ app/models/channels/rss_channel.rb
@@ -25,7 +25,7 @@ class RssChannel < Channel
           description: web_item.summary,
           link: Link.where(url: web_item.url).first_or_create,
           published_at: web_item.published,
-          digest: Digest::SHA256.base64digest(web_item.title + web_item.content + web_item.summary)
+          digest: Digest::SHA256.base64digest(web_item.title.to_s + web_item.content.to_s + web_item.summary.to_s)
         )
         i.assets.build(url: web_item.image) if web_item.image
         i.tag_names = (web_item.categories) if web_item.categories
