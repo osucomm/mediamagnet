a7ec4eb8c02ad24389c2e9507a4f043dada5150c
Mike Butsko <butsko.7@osu.edu>
2015-03-17 11:32:38 -0400

    Forgot job class oops

---
+++ app/jobs/refresh_channel_job.rb
@@ -0,0 +1,20 @@
+class RefreshChannelJob < Struct.new(:channel)
+  def perform
+    # Only actually run if our job has this channel locked. Otherwise silently
+    # skip.
+    if @job.id == channel.refresh_lock.job_id
+      channel.refresh_items
+    end
+    true
+  end
+
+  def before(job)
+    @job = job
+    channel.lock(job_id: job.id)
+  end
+
+  def success(job)
+    # Remove lock if this is really our job.
+    channel.unlock if channel.refresh_lock && channel.refresh_lock.id == job.id
+  end
+end
