bb2545d4f6b8049df843acecf634cae539f37a67
Mike Butsko <butsko.7@osu.edu>
2015-03-30 14:06:56 -0400

    Ship cache for non-api urls

---
+++ app/middleware/skip_cache.rb
@@ -0,0 +1,14 @@
+class SkipCache
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+
+    unless env['REQUEST_URI'] =~ /^\/api\/v/
+      env["rack-cache.force-pass"] = true
+    end
+
+    @app.call(env)
+  end
+end
