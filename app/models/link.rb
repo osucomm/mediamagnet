e2e4076ee473b55e577955079c1c1d108162fd4b
Mike Butsko <butsko.7@osu.edu>
2015-03-31 13:06:43 -0400

    Get instead of head for speed

--- app/models/link.rb
+++ app/models/link.rb
@@ -12,18 +12,18 @@ class Link < ActiveRecord::Base
     def resolve_uri(url, last_url = nil)
 
       # If it's an absolute URL, use last known host. [RFC3986], Section 4.2
-      if (url.match PATTERN).nil?
+      if (url.match PATTERN).nil? && last_url.present?
         url = last_url.host + url.to_s
       end
 
-      uri = URI(URI.encode(url.strip))
+      uri = URI(URI.encode(self.decode(url.strip)))
       return_url = ''
 
       begin
         Net::HTTP.start(uri.host, uri.port, open_timeout: 10,
                           :use_ssl => uri.scheme == 'https') do |http|
           begin
-            request = Net::HTTP::Get.new uri
+            request = Net::HTTP::Head.new uri
             response = http.request request # Net::HTTPResponse object
             if response.code.to_s.match(/^3/)
               return_url = resolve_uri(response['location'], uri)
@@ -59,6 +59,12 @@ class Link < ActiveRecord::Base
       rescue
         false
       end
+    end
+
+    def decode(uri)
+      another_round = URI.decode(uri)
+      self.decode(another_round) if another_round != uri
+      another_round
     end
 
   end
