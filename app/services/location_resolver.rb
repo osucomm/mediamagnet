300ad80ef507ad5a30ce6876fa86eb1799ab3c88
Mike Butsko <butsko.7@osu.edu>
2015-04-27 09:18:50 -0400

    Fix for nil location

--- app/services/location_resolver.rb
+++ app/services/location_resolver.rb
@@ -1,15 +1,13 @@
 class LocationResolver
   def initialize(location)
-    @location = URI.encode(location)
+    @location = location.nil? ? nil : URI.encode(location)
   end
 
   def resolve
-    unless @location.nil?
-      base_url = 'http://newmedia.osu.edu/projects/osu_location/public/?q='
-      response = HTTParty.get(base_url + @location).parsed_response
-      JSON.parse(response).keep_if do |k,v|
-        [:code, :name, :address, :latitude, :longitude].include?(k.to_sym)
-      end
+    base_url = 'http://newmedia.osu.edu/projects/osu_location/public/?q='
+    response = HTTParty.get(base_url + @location.to_s).parsed_response
+    JSON.parse(response).keep_if do |k,v|
+      [:code, :name, :address, :latitude, :longitude].include?(k.to_sym)
     end
   end
 end
