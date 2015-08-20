61a6dfcbc3d34c7b702054379299fafcc37d6fbc
Mike Butsko <butsko.7@osu.edu>
2015-08-20 13:20:53 -0400

    Forgot protocol

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "#{Figaro.env.REDIS_HOST}:#{Figaro.env.REDIS_PORT}" }
+  config.redis = { url: "redis://#{Figaro.env.REDIS_HOST}:#{Figaro.env.REDIS_PORT}" }
 end
 Sidekiq.configure_client do |config|
-  config.redis = { url: "#{Figaro.env.REDIS_HOST}:#{Figaro.env.REDIS_PORT}" }
+  config.redis = { url: "redis://#{Figaro.env.REDIS_HOST}:#{Figaro.env.REDIS_PORT}" }
 end
