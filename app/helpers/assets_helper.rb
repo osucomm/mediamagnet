e209430c7f009c71cf53e7f9930a8d3a2609219d
Mike Butsko <butsko.7@osu.edu>
2015-09-28 09:57:09 -0400

    Catch blank url

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -1,13 +1,15 @@
 module AssetsHelper
 
   def render_asset(asset, custom_class=nil)
-    case(asset.mime.split('/').first)
-    when 'image'
-      content_tag(:img, nil, src: asset.url, alt: asset.alt, title: asset.title, class: ['media-object', custom_class].join(' '))
-    when 'video'
-      content_tag(:video, nil, src: asset.url, controls: true)
-    else
-      link_to asset.url, asset.url
+    if asset.mime
+      case(asset.mime.split('/').first)
+      when 'image'
+        content_tag(:img, nil, src: asset.url, alt: asset.alt, title: asset.title, class: ['media-object', custom_class].join(' '))
+      when 'video'
+        content_tag(:video, nil, src: asset.url, controls: true)
+      else
+        link_to asset.url, asset.url
+      end
     end
   end
 
