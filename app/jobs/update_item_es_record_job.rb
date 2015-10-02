38ff91be7156c721837a38868fcfded65d700464
Mike Butsko <butsko.7@osu.edu>
2015-10-02 09:05:16 -0400

    Rescue from deserialization error

--- app/jobs/update_item_es_record_job.rb
+++ app/jobs/update_item_es_record_job.rb
@@ -1,6 +1,10 @@
 class UpdateItemEsRecordJob < ActiveJob::Base
   queue_as :default
 
+  rescue_from ActiveJob::DeserializationError do |exception|
+    logger.debug "Tried to update a record that was deleted"
+  end
+
   def perform(item)
     item.update_es_record
   end
