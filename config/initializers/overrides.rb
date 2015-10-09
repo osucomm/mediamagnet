01f0800f05737f978a366333893c8d289a608ee6
Corey Hinshaw <hinshaw.25@osu.edu>
2015-10-09 11:42:07 -0400

    Fix class namespace

--- config/initializers/overrides.rb
+++ config/initializers/overrides.rb
@@ -7,7 +7,7 @@ ActiveJob::QueueAdapters::SidekiqAdapter.class_eval do
     def enqueue(job) #:nodoc:
       #Sidekiq::Client does not support symbols as keys
       job.provider_job_id = Sidekiq::Client.push \
-        'class'   => JobWrapper,
+        'class'   => ActiveJob::QueueAdapters::SidekiqAdapter::JobWrapper,
         'wrapped' => job.class.to_s,
         'queue'   => job.queue_name,
         'args'    => [ job.serialize ]
@@ -15,7 +15,7 @@ ActiveJob::QueueAdapters::SidekiqAdapter.class_eval do
 
     def enqueue_at(job, timestamp) #:nodoc:
       job.provider_job_id = Sidekiq::Client.push \
-        'class'   => JobWrapper,
+        'class'   => ActiveJob::QueueAdapters::SidekiqAdapter::JobWrapper,
         'wrapped' => job.class.to_s,
         'queue'   => job.queue_name,
         'args'    => [ job.serialize ],
